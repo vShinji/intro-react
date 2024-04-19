e4ff3bb6c712ba0eeaee208069435d1442896cf3
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-04-19 16:37:25 +0900

    eslint setup done

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,35 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet"
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Minnie",
-      animal: "Dog",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Mel",
-      animal: "Dog",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Raiden",
-      animal: "Dog",
-      breed: "Mixed",
-    }),
-  ]);
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Minnie" animal="Dog" breed="Mixed"/>
+      <Pet name="Mel" animal="Dog" breed="Mixed"/>
+      <Pet name="Raiden" animal="Dog" breed="Mixed"/>
+    </div>
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
