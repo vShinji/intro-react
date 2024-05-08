a080b9465fd0e106ecb0cbe4f736c494dc6946fd
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-05-09 05:03:10 +0900

    React router

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,15 +1,20 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams/>
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
